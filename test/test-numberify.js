ca6c681e5aa71200c95805088765860458b8d838
dan1wang <38196313+dan1wang@users.noreply.github.com>
2019-03-25 12:13:42 +0800

    Mocha unit test

---
+++ test/test-numberify.js
@@ -0,0 +1,114 @@
+'use strict';
+
+/* eslint no-var: off */
+
+var conversely = require('../build/index.js');
+var assert = require('chai').assert;
+
+describe('numberify()', function() {
+  context('with function as argument', function() {
+    it('should convert () => NaN to null', function() {
+      assert.equal( conversely.numberify( fnNumberNaN ), null);
+    });
+    it('should convert () => 0 to 0', function() {
+      assert.equal( conversely.numberify( fnNumberZero ), 0);
+    });
+    it('should convert () => 1 to 2', function() {
+      assert.equal( conversely.numberify( fnNumberOne ), 1);
+    });
+    it('should convert () => 2 to 2', function() {
+      assert.equal( conversely.numberify( fnNumberTwo ), 2);
+    });
+    it('should convert () => "Huh?" to null', function() {
+      assert.equal( conversely.numberify( fnStringHuh ), null);
+    });
+    it('should convert () => "0" to 0', function() {
+      assert.equal( conversely.numberify( fnStringZero ), 0);
+    });
+    it('should convert () => "1" to 1', function() {
+      assert.equal( conversely.numberify( fnStringOne ), 1);
+    });
+    it('should convert () => "2" to null', function() {
+      assert.equal( conversely.numberify( fnStringTwo ), 2);
+    });
+    it('should convert () => true to 1', function() {
+      assert.equal( conversely.numberify( fnBooleanTrue ), true);
+    });
+    it('should convert () => false to 0', function() {
+      assert.equal( conversely.numberify( fnBooleanFalse ), 0);
+    });
+    it('should convert () => null to null', function() {
+      assert.equal( conversely.numberify( fnNull ), null);
+    });
+  });
+  context('with object as argument', function() {
+    it('should convert new WhatYouSaid(NaN)  to null', function() {
+      assert.equal( conversely.numberify( oNumberNaN ), null);
+    });
+    it('should convert new WhatYouSaid(0) to 0', function() {
+      assert.equal( conversely.numberify( oNumberZero ), 0);
+    });
+    it('should convert new WhatYouSaid(1) to 1', function() {
+      assert.equal( conversely.numberify( oNumberOne ), 1);
+    });
+    it('should convert new WhatYouSaid(2) to 2', function() {
+      assert.equal( conversely.numberify( oNumberTwo ), 2);
+    });
+    it('should convert new WhatYouSaid("Huh?") to null', function() {
+      assert.equal( conversely.numberify( oStringHuh ), null);
+    });
+    it('should convert new WhatYouSaid("0") to 0', function() {
+      assert.equal( conversely.numberify( oStringZero ), 0);
+    });
+    it('should convert new WhatYouSaid("1") to 1', function() {
+      assert.equal( conversely.numberify( oStringOne ), 1);
+    });
+    it('should convert new WhatYouSaid("2") to null', function() {
+      assert.equal( conversely.numberify( oStringTwo ), 2);
+    });
+    it('should convert new WhatYouSaid(true) to 1', function() {
+      assert.equal( conversely.numberify( fnBooleanTrue ), true);
+    });
+    it('should convert new WhatYouSaid(false) to 0', function() {
+      assert.equal( conversely.numberify( fnBooleanFalse ), 0);
+    });
+    it('should convert new WhatYouSaid(null) to null', function() {
+      assert.equal( conversely.numberify( fnNull ), null);
+    });
+  });
+  context('with function returning object as argument', function() {
+    it('should convert new WhatYouSaid(NaN)  to null', function() {
+      assert.equal( conversely.numberify( fnObjNumberNaN ), null);
+    });
+    it('should convert () => new WhatYouSaid(0) to 0', function() {
+      assert.equal( conversely.numberify( fnObjNumberZero ), 0);
+    });
+    it('should convert () => new WhatYouSaid(1) to 1', function() {
+      assert.equal( conversely.numberify( fnObjNumberOne ), 1);
+    });
+    it('should convert () => new WhatYouSaid(2) to 2', function() {
+      assert.equal( conversely.numberify( fnObjNumberTwo ), 2);
+    });
+    it('should convert () => new WhatYouSaid("Huh?") to null', function() {
+      assert.equal( conversely.numberify( fnObjStringHuh ), null);
+    });
+    it('should convert () => new WhatYouSaid("0") to 0', function() {
+      assert.equal( conversely.numberify( fnObjStringZero ), 0);
+    });
+    it('should convert () => new WhatYouSaid("1") to 1', function() {
+      assert.equal( conversely.numberify( fnObjStringOne ), 1);
+    });
+    it('should convert () => new WhatYouSaid("2") to null', function() {
+      assert.equal( conversely.numberify( fnObjStringTwo ), 2);
+    });
+    it('should convert () => new WhatYouSaid(true) to 1', function() {
+      assert.equal( conversely.numberify( fnObjBooleanTrue ), true);
+    });
+    it('should convert () => new WhatYouSaid(false) to 0', function() {
+      assert.equal( conversely.numberify( fnObjBooleanFalse ), 0);
+    });
+    it('should convert () => new WhatYouSaid(null) to null', function() {
+      assert.equal( conversely.numberify( fnObjNull ), null);
+    });
+  });
+});
---
+++ test/test-setup.js
@@ -0,0 +1,47 @@
+/* eslint brace-style: ["error", "1tbs", { "allowSingleLine": true }] */
+global.fnNumberNaN = function() {return NaN;};
+global.fnNumberZero = function() {return 0;};
+global.fnNumberOne = function() {return 1;};
+global.fnNumberTwo = function() {return 2;};
+global.fnStringHuh = function() {return 'Huh?';};
+global.fnStringZero = function() {return '0';};
+global.fnStringOne = function() {return '1';};
+global.fnStringTwo = function() {return '2';};
+global.fnBooleanTrue = function() {return true;};
+global.fnBooleanFalse = function() {return false;};
+global.fnNull = function() {return null;};
+
+/**
+ * Create an object with valueOf() method that echos the initial value.
+ * @param {Any} value Initial value
+ * @return {Object}
+ */
+function WhatYouSaid(value) {
+  this['value'] = value;
+  this['valueOf'] = function() {return this.value;};
+  return this;
+}
+
+global.oNumberNaN = new WhatYouSaid(NaN);
+global.oNumberZero = new WhatYouSaid(0);
+global.oNumberOne = new WhatYouSaid(1);
+global.oNumberTwo = new WhatYouSaid(2);
+global.oStringHuh = new WhatYouSaid('Huh?');
+global.oStringZero = new WhatYouSaid('0');
+global.oStringOne = new WhatYouSaid('1');
+global.oStringTwo = new WhatYouSaid('2');
+global.oBooleanTrue = new WhatYouSaid(true);
+global.oBooleanFalse = new WhatYouSaid(false);
+global.oNull = new WhatYouSaid(null);
+
+global.fnObjNumberNaN = function() {return global.oNumberNaN;};
+global.fnObjNumberZero = function() {return global.oNumberZero;};
+global.fnObjNumberOne = function() {return global.oNumberOne;};
+global.fnObjNumberTwo = function() {return global.oNumberTwo;};
+global.fnObjStringHuh = function() {return global.oStringHuh;};
+global.fnObjStringZero = function() {return global.oStringZero;};
+global.fnObjStringOne = function() {return global.oStringOne;};
+global.fnObjStringTwo = function() {return global.oStringTwo;};
+global.fnObjBooleanTrue = function() {return global.oBooleanTrue;};
+global.fnObjBooleanFalse = function() {return global.oBooleanFalse;};
+global.fnObjNull = function() {return global.oNull;};
